1cdeb4a119855e1eb7a9527219df311021ac3b43
Brandon <bherford@provlib.org>
2017-06-15 09:02:02 -0400

    initial commit to PPL code_snippets repository

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'http://localhost/code_snippets/'
+		}
+	});
+
+});
+
+gulp.task('sass', function() {
+	return gulp.src('scss/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('css/'))
+	.pipe(browserSync.reload({
+		stream:true
+	}))
+});
+
+gulp.task('watch', ['browserSync', 'sass'],  function() {
+	gulp.watch('scss/**/*.scss', ['sass']);
+	gulp.watch('*.html', browserSync.reload );
+	gulp.watch('js/main.js', browserSync.reload);
+	gulp.watch('js/**/*.json', browserSync.reload);
+	gulp.watch('images/**/*', browserSync.reload);
+});
